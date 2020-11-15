e8247124fa43ca9008a185a69a332c7231c5303e
Vyacheslav Vaschenok <vyacheslav.vaschenok@nike.com>
2020-11-15 22:52:03 +0300

    init import-service

---
+++ import-service/serverless.ts
@@ -0,0 +1,63 @@
+import { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'import-service',
+    // app and org for use with dashboard.serverless.com
+    // app: your-app-name,
+    // org: your-org-name,
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 's3:ListBucket',
+        Resource: 'arn:aws:s3:::aws-nodejs-product-service-storage'
+      },
+      {
+        Effect: 'Allow',
+        Action: 's3:*',
+        Resource: 'arn:aws:s3:::aws-nodejs-product-service-storage/*'
+      }
+    ]
+  },
+  functions: {
+    hello: {
+      handler: 'handler.importProductsFile',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'import',
+            request: {
+              parameters:{
+                querystrings:{
+                  name: true
+                }
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
